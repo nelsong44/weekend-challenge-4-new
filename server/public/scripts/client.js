8dab32a0d0f1be134d871d5c35710adf777b33c4
nelsong44 <nelsong44@gmail.com>
2017-07-14 16:24:54 -0500

    created README.md, scoped myApp in html and created a controller

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,10 @@
 console.log('JS sourced');
+
+//create angular app
+var myApp = angular.module('myApp', []);
+
+//link app to controller
+myApp.controller('galleryController', function() {
+  //scoping gallery to this controller
+  var gallery = this;
+});//end gallery controller
--- server/server.js
+++ server/server.js
@@ -2,17 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
-var path = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
-
-// app.get('/*', function(req, res) {
-//   var file = req.params[0] || 'views/index.html';
-//   res.sendFile(path.join(__dirname, 'public', file));
-// });
 
 app.listen(port, function(){
   console.log("Listening on port: ", port);
