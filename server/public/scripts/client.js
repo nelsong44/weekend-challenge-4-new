61e32b7f4c82b4cd08cb459aa7c2d72670220432
nelsong44 <nelsong44@gmail.com>
2017-07-16 12:08:25 -0500

    display of images and their descriptions on DOM being toggled on click of their div, counter button and counter created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,29 @@ console.log('JS sourced');
 var myApp = angular.module('myApp', []);
 
 //link app to controller
-myApp.controller('galleryController', function() {
+myApp.controller('GalleryController', function() {
   //scoping gallery to this controller
   var gallery = this;
+  gallery.imageArray = [
+    //store relative image paths
+    {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"},
+    {image: "images/anderAndLucy.jpg", description: "My brother, Ander, with his newborn daughter, Lucy"},
+    {image: "images/premie.jpg", description: "Me at Children's Hospital at 4 weeks old being held by my mom"},
+    {image: "images/turtle.jpg", description: "My pup, Turtle"}
+    // {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"},
+    // {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"}
+  ];
+
+  //increase likes count by 1 on click of button
+  gallery.likes = 0;
+  gallery.likesCounter = function() {
+    gallery.likes += 1;
+  };//end likesCounter
+
+ //toggle display of image and description on click of div
+  gallery.display = true;
+  gallery.toggleDisplay = function() {
+    gallery.display = !gallery.display;
+  };//end toogleDisplay
+
 });//end gallery controller
