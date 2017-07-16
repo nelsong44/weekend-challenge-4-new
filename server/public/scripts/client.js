79e9264f269193bd34fcfc9e082141e7f7a119f1
nelsong44 <nelsong44@gmail.com>
2017-07-16 14:09:53 -0500

    counter working, divs staying stationary as the images and descriptions are toggled, baase formatting completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,24 +9,22 @@ myApp.controller('GalleryController', function() {
   var gallery = this;
   gallery.imageArray = [
     //store relative image paths
-    {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"},
-    {image: "images/anderAndLucy.jpg", description: "My brother, Ander, with his newborn daughter, Lucy"},
-    {image: "images/premie.jpg", description: "Me at Children's Hospital at 4 weeks old being held by my mom"},
-    {image: "images/turtle.jpg", description: "My pup, Turtle"}
-    // {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"},
-    // {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London"}
+    {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London", likes: 0, display: true},
+    {image: "images/anderAndLucy.jpg", description: "My brother, Ander, with his newborn daughter, Lucy", likes: 0, display: true},
+    {image: "images/premie.jpg", description: "Me at Children's Hospital at 4 weeks old being held by my mom", likes: 0, display: true},
+    {image: "images/turtle.jpg", description: "My pup, Turtle", likes: 0, display: true},
+    {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London", likes: 0, display: true},
+    {image: "images/alAndGary.jpg", description: "My sister, Alex, and her husband, Gary, in London", likes: 0, display: true}
   ];
 
   //increase likes count by 1 on click of button
-  gallery.likes = 0;
-  gallery.likesCounter = function() {
-    gallery.likes += 1;
+  gallery.likesCounter = function(galleryObj) {
+    galleryObj.likes += 1;
   };//end likesCounter
 
  //toggle display of image and description on click of div
-  gallery.display = true;
-  gallery.toggleDisplay = function() {
-    gallery.display = !gallery.display;
+  gallery.toggleDisplay = function(galleryObj) {
+    galleryObj.display = !galleryObj.display;
   };//end toogleDisplay
 
 });//end gallery controller
